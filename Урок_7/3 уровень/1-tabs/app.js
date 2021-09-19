43a0447389455fc1d55b82de47694fbca1616941
Andrey <andrei_sapego@mail.ru>
2021-09-19 23:10:11 +0300

    Домашняя работа 7

---
+++ Урок_7/3 уровень/1-tabs/app.js
@@ -0,0 +1,71 @@
+'use strict';
+const texts = {
+    text1: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    text2: 'Далеко-далеко за словесными горами в стране гласных и согласных живут рыбные тексты.',
+    text3: 'Проснувшись однажды утром после беспокойного сна, Грегор Замза обнаружил.'
+};
+
+/* 
+1. Получите ссылку на .text, например с помощью querySelector
+2. Получите коллекцию, в которой хранятся все .nav-link, например с помощью querySelectorAll
+    2.1 Переберите полученную коллекцию, например с помощью forEach, и каждой ссылке назначьте
+    обработчик клика функцию clickHandler.
+*/
+
+let cont = document.querySelector('.text');
+let link_items = document.querySelectorAll('.nav-link');
+
+link_items.forEach(item => {
+    clickHandler(item);
+});
+
+
+/**
+ * Обработчик клика по .nav-link
+ * @param {MouseEvent} event 
+ */
+function clickHandler(event) {
+    // здесь вызывайте changeText и changeActiveClass, и передавайте
+    // им объект события.
+    event.addEventListener('click', change => {
+        changeText(change);
+        changeActiveClass(change);
+    });
+}
+
+/**
+ * Эта функция должна убирать .active у предыдущего .nav-link и ставить его
+ * на тот, по которому кликнули.
+ * @param {MouseEvent} event 
+ */
+function changeActiveClass(event) {
+
+    link_items.forEach(item => {
+        item.classList.remove('active');
+    });
+
+    event.target.classList.add('active');
+}
+
+/**
+ * Эта фукнция должна читать текст (например через textContent) из 
+ * .nav-link по которому кликнули и в зависимости от этого в .text
+ * ставить соответствующий текст из texts.
+ * @param {MouseEvent} event 
+ */
+function changeText(event) {
+    let num = event.currentTarget.textContent.split(' ');
+
+    switch (num[1]) {
+        case '1':
+            cont.innerHTML = texts.text1;
+            break;
+        case '2':
+            cont.innerHTML = texts.text2;
+            break;
+        case '3':
+            cont.innerHTML = texts.text3;
+            break;
+
+    }
+}
